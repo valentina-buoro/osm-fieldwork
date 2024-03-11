6e18dc7b839aafd5cc0902f3b1e99e453a49c76f
valentina buoro <valentinabuoro4@gmail.com>
2024-03-11 15:11:16 +0100

    fix: removed hard coded filepath and replaced with a dynamic file path so that the file path can match users file path

--- osm_fieldwork/outreachy.py
+++ osm_fieldwork/outreachy.py
@@ -1,9 +1,11 @@
+import os
 from io import BytesIO
 from osm_fieldwork.basemapper import create_basemap_file
 
 
 def main():
-    with open("/Users/valentinabuoro/outreachy/osm-fieldwork/tests/testdata/Rollinsville.geojson", "rb") as geojson_file:
+    boundary = f"{os.path.dirname(os.path.abspath(__file__))}/../tests/testdata/Rollinsville.geojson"
+    with open(boundary, "rb") as geojson_file:
         boundary = geojson_file.read()  # read as a `bytes` object.
         boundary_bytesio = BytesIO( boundary)  # add to a BytesIO wrapper
 
